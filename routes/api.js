f5085c50b63050f6994368b66e5e1752a96de464
Sami Fink <fink.s.m.8651@gmail.com>
2024-06-08 12:17:01 +0000

    feat: added db schema

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,26 @@
 'use strict';
+require('dotenv').config();
+const mongoose = require('mongoose');
 
+// connect to DB
+mongoose.connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+
+// board schema model
+let boardSchema = new mongoose.Schema({
+  text: String,
+  created_on: Date,
+  bumped_on: Date,
+  reported: Boolean,
+  deletePassword: String,
+  replies: [{
+    text: String, 
+    created_on: Date,
+    deletePassword: String,
+    reported: Boolean
+  }]
+});
+
+let Board = mongoose.model('Board', boardSchema);
 
 module.exports = function (app) {
   
